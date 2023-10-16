6da3aa1f07f33154b46c3a8ae5ebcfc99eafd1ee
Villy15 <adrianvill07@gmail.com>
2023-10-16 14:41:00 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/s15/campanilla/villanueva/playbach/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.s15.campanilla.villanueva.playbach;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
