6f8f6eafa0a52948a6cb2e267aa3a17fbc1cddd6
jaz8c <justice_campanilla@dlsu.edu.ph>
2023-10-25 18:40:38 +0800

    Edited Register Page

--- app/src/main/java/com/example/s15/campanilla/villanueva/playbach/RegisterActivity.java
+++ app/src/main/java/com/example/s15/campanilla/villanueva/playbach/RegisterActivity.java
@@ -4,18 +4,22 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
     Button registerButton;
+    TextView login;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
 
         registerButton = findViewById(R.id.buttonRegister);
+        login = findViewById(R.id.login);
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +28,15 @@ public class RegisterActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
     }
 }
