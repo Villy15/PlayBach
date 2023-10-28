e3c9a8208fd8f7961bf67e0fb22ac7e161d6fff6
Villy15 <adrianvill07@gmail.com>
2023-10-28 16:43:29 +0800

    Add working Navbar

--- app/src/main/java/com/example/s15/campanilla/villanueva/playbach/MainActivity.java
+++ app/src/main/java/com/example/s15/campanilla/villanueva/playbach/MainActivity.java
@@ -1,94 +1,61 @@
 package com.example.s15.campanilla.villanueva.playbach;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.TextView;
+import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import com.example.s15.campanilla.villanueva.playbach.fragments.HomeFragment;
+import com.example.s15.campanilla.villanueva.playbach.fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstants;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener;
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
-    YouTubePlayerView youTubePlayerView;
-    TextView playbackText;
     BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        youTubePlayerView = findViewById(R.id.youtube_player_view);
-
-        playbackText = findViewById(R.id.playbackText);
 
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
-        initYouTubePlayerView(youTubePlayerView);
+        funBottomNavigationView();
     }
 
-//    This function initializes the YouTubePlayerView.
-    private void initYouTubePlayerView (YouTubePlayerView youTubePlayerView) {
-        getLifecycle().addObserver(youTubePlayerView);
+//    Gathered from https://gist.github.com/codinginflow/8a728a27a78e92876ca1c71b3dce28f6
+    private void funBottomNavigationView() {
+        // Set Default item: when app open
+        bottomNavigationView.setSelectedItemId(R.id.nav_home);
+        // Set Default fragment
+        loadNavFragment(new HomeFragment());
 
-        youTubePlayerView.addYouTubePlayerListener(new AbstractYouTubePlayerListener() {
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
             @Override
-            public void onReady(@NonNull YouTubePlayer youTubePlayer) {
-                String videoId = "IwInqrN_auU";
-                youTubePlayer.loadVideo(videoId, 0);
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
 
-                setPlaybackSpeedButtonsClickListeners(youTubePlayer);
+                if (id == R.id.nav_home) {
+                    loadNavFragment(new HomeFragment());
+                } else if (id == R.id.nav_profile) {
+                    loadNavFragment(new ProfileFragment());
+                }
+
+                return true;
             }
-
-            @Override
-            public void onPlaybackRateChange(@NonNull YouTubePlayer youTubePlayer, @NonNull PlayerConstants.PlaybackRate playbackRate) {
-                super.onPlaybackRateChange(youTubePlayer, playbackRate);
-
-                String playbackRateString = getPlaybackSpeedString(playbackRate);
-                playbackText.setText(String.format("Playback: %s", playbackRateString));
-            }
-
         });
     }
 
-//    This function sets the click listeners for the playback speed buttons.
-    private void setPlaybackSpeedButtonsClickListeners (YouTubePlayer youTubePlayer) {
-        Button playbackBtn1 = findViewById(R.id.playbackBtn1);
-        Button playbackBtn2 = findViewById(R.id.playbackBtn2);
-        Button playbackBtn3 = findViewById(R.id.playbackBtn3);
-        Button playbackBtn4 = findViewById(R.id.playbackBtn4);
+    private void loadNavFragment(Fragment fragment) {
+        FragmentManager fragmentManager= getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction= fragmentManager.beginTransaction();
 
-
-        playbackBtn1.setOnClickListener(view -> youTubePlayer.setPlaybackRate(PlayerConstants.PlaybackRate.RATE_0_25));
-        playbackBtn2.setOnClickListener(view -> youTubePlayer.setPlaybackRate(PlayerConstants.PlaybackRate.RATE_0_5));
-        playbackBtn3.setOnClickListener(view -> youTubePlayer.setPlaybackRate(PlayerConstants.PlaybackRate.RATE_1));
-        playbackBtn4.setOnClickListener(view -> youTubePlayer.setPlaybackRate(PlayerConstants.PlaybackRate.RATE_2));
-    }
-
-//    This function returns the playback speed string to a format from RATE_1 to 1x.
-    private String getPlaybackSpeedString (PlayerConstants.PlaybackRate playbackRate) {
-        String playbackRateString = playbackRate.toString();
-
-        switch (playbackRateString) {
-            case "RATE_0_25":
-                playbackRateString = "0.25x";
-                break;
-            case "RATE_0_5":
-                playbackRateString = "0.5x";
-                break;
-            case "RATE_1":
-                playbackRateString = "1x";
-                break;
-            case "RATE_2":
-                playbackRateString = "2x";
-                break;
-        }
-
-        return playbackRateString;
+        //use replace
+        fragmentTransaction.replace(R.id.flFragment, fragment);
+        fragmentTransaction.commit();
     }
 }
