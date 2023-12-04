e3c73613f66f419374bfe1fedbc8b3368e539629
Villy15 <adrianvill07@gmail.com>
2023-12-04 18:28:56 +0800

    Edit songs

--- app/src/main/java/com/example/s15/campanilla/villanueva/playbach/CommunityAdapter.java
+++ app/src/main/java/com/example/s15/campanilla/villanueva/playbach/CommunityAdapter.java
@@ -1,5 +1,89 @@
 package com.example.s15.campanilla.villanueva.playbach;
 
-public class CommunityAdapter {
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.s15.campanilla.villanueva.playbach.Classes.Songs;
+import com.example.s15.campanilla.villanueva.playbach.fragments.HomeFragment;
+
+import java.util.List;
+
+public class CommunityAdapter extends RecyclerView.Adapter<CommunityAdapter.ViewHolder> {
+    private static final String PREFS_NAME = "CommunityAdapterPreferences";
+
+    private List<Songs> songsList;
+
+    public CommunityAdapter(List<Songs> songsList) {
+            this.songsList = songsList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.comm_song_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Songs song = songsList.get(position);
+        holder.songTitle.setText(song.getTitle());
+        // Set other attributes like image
+
+        Glide.with(holder.itemView.getContext())
+                .load(song.getThumbnail())
+                .into(holder.songImage);
+
+        holder.songContributor.setText("Practiced by " + song.getContributor());
+
+
+        // Set the click listener
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = holder.itemView.getContext();
+                SharedPreferences preferences = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = preferences.edit();
+                editor.putString("youtubeUrl", song.getYoutubeUrl());
+                editor.apply();
+
+                Log.d("SongsAdapter", "YouTube URL set in SharedPreferences: " + song.getYoutubeUrl());
+
+                MainActivity mainActivity = (MainActivity) context;
+                mainActivity.loadNavFragment(new HomeFragment());
+
+                mainActivity.bottomNavigationView.setSelectedItemId(R.id.nav_home);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return songsList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        TextView songTitle;
+        ImageView songImage;
+
+        TextView songContributor;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            songTitle = itemView.findViewById(R.id.songTitle);
+            songImage = itemView.findViewById(R.id.songImage);
+            songContributor = itemView.findViewById(R.id.songContributor);
+        }
+    }
 }
--- app/src/main/java/com/example/s15/campanilla/villanueva/playbach/fragments/CommunityFragment.java
+++ app/src/main/java/com/example/s15/campanilla/villanueva/playbach/fragments/CommunityFragment.java
@@ -5,26 +5,72 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.s15.campanilla.villanueva.playbach.Classes.Songs;
+import com.example.s15.campanilla.villanueva.playbach.CommunityAdapter;
 import com.example.s15.campanilla.villanueva.playbach.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CommunityFragment extends Fragment {
+    private RecyclerView songsRecyclerView;
+    private CommunityAdapter communityAdapter;
+    private List<Songs> songsList = new ArrayList<>();
+
 
     public CommunityFragment() {
         // Required empty public constructor
     }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_community, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        songsRecyclerView = view.findViewById(R.id.songsRecyclerView);
+        songsRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        populateSongsList();
+
+        communityAdapter = new CommunityAdapter(songsList);
+        songsRecyclerView.setAdapter(communityAdapter);
+    }
+
+    private void populateSongsList () {
+        songsList.add(new Songs(
+                        "Littleroot Town - Pokemon",
+                        "vbLu5O8rxQk",
+                        "https://img.youtube.com/vi/vbLu5O8rxQk/0.jpg",
+                        "Adrian Villanueva"
+                )
+        );
+
+        songsList.add(new Songs(
+                        "Persona 5 - Life Will Change (中英歌詞)",
+                        "CGwH6rZk7VM",
+                        "https://img.youtube.com/vi/CGwH6rZk7VM/0.jpg",
+                        "Adrian Villanueva"
+                )
+        );
+
+        songsList.add(new Songs(
+                        "I Really Want to Stay At Your House",
+                        "h4VJGNNSQnw",
+                        "https://img.youtube.com/vi/h4VJGNNSQnw/0.jpg",
+                        "Adrian Villanueva"
+                )
+        );
+    }
+
+
 }
