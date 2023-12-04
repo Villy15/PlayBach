f662dadccddaf87dc30270704e85146ec9489345
Villy15 <adrianvill07@gmail.com>
2023-12-04 17:07:00 +0800

    Add signout

--- app/src/main/java/com/example/s15/campanilla/villanueva/playbach/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/s15/campanilla/villanueva/playbach/fragments/ProfileFragment.java
@@ -1,15 +1,24 @@
 package com.example.s15.campanilla.villanueva.playbach.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
 
+import com.example.s15.campanilla.villanueva.playbach.LoginActivity;
 import com.example.s15.campanilla.villanueva.playbach.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
+
+    Button logoutButton;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -19,5 +28,37 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        logoutButton = view.findViewById(R.id.logoutButton);
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Log out the current user
+                FirebaseAuth.getInstance().signOut();
+
+                // Google sign out
+                GoogleSignInClient googleSignInClient = GoogleSignIn.getClient(getActivity(), GoogleSignInOptions.DEFAULT_SIGN_IN);
+                googleSignInClient.signOut().addOnCompleteListener(getActivity(), task -> {
+                    // Go to LoginActivity after signing out
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear the back stack
+                    startActivity(intent);
+                });
+
+                // Go to LoginActivity
+                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK); // Clear the back stack
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
